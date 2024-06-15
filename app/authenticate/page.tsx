2e993af60b35df3ef416f8a1125f29bd0be828de
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 01:01:27 +1000

    Updated authentication
    Created authentication form component and added form to authenticate page

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -1,3 +1,13 @@
+import Form from "../ui/authenticate/create-form";
+import { lusitana } from "../ui/fonts";
+
 export default function Page() {
-    return <p>Authenticate Page</p>
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Twitch Authentication
+            </h1>
+            <Form />
+        </main>
+    );
 }
