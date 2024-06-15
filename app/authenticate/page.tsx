958e79e4e64b73e4385808fe85b0b1b2b755a52e
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 03:09:17 +1000

    Update authenticate
    Updated font

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -1,10 +1,10 @@
 import Form from "../ui/authenticate/create-form";
-import { lusitana } from "../ui/fonts";
+import { inter } from "../ui/fonts";
 
 export default function Page() {
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
                 Twitch Authentication
             </h1>
             <Form />
