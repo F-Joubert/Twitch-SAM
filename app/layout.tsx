a3a0b796625b520545cd8412cd8caa80cfdbf0ae
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 17:37:01 +1000

    Update layout.tsx
    Made twitch chat listener part of global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideNav from "@/app/ui/sidenav";
+import TwitchChatListener from "./ui/twitch/twitch-listener";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
               <div className="w-full flex-none md:w-64">
                   <SideNav />
+                  <TwitchChatListener />
               </div>
               <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
           </div>
