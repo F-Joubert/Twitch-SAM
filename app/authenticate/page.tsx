30710aad19c70fb757140688f30354bc6bb4d591
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 21:15:30 +1000

    Update page.tsx
    Updated title styling

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -4,7 +4,7 @@ import { inter } from "../ui/fonts";
 export default function Page() {
     return (
         <main>
-            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`${inter.className} mb-10 text-xl md:text-3xl pb-4 border-b-2 border-fuchsia-950`}>
                 Twitch Authentication
             </h1>
             <Form />
