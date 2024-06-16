1c7145cd2be4c8816386910c4dae96eb5d11a0d2
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 20:58:50 +1000

    Restyled titles
    Restyled titles on playground and settings pages

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -4,7 +4,7 @@ import { inter } from "@/app/ui/fonts";
 export default function Page() {
     return (
         <div>
-            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`${inter.className} mb-10 text-xl md:text-3xl pb-4 border-b-2 border-fuchsia-950`}>
                 Playground
             </h1>
             <Form />
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,10 +6,12 @@ import SettingsPage from "../ui/settings/settings-form";
 export default function Page() {
     return (
         <div>
-            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`${inter.className} mb-10 text-xl md:text-3xl pb-4 border-b-2 border-fuchsia-950`}>
                     App Settings
             </h1>
-            <SettingsPage />
+            <div className="w-full">
+                <SettingsPage />
+            </div>
         </div>
     )
 }
