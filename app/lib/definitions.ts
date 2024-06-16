c3cc89fe6bbc1d9d4b7dcafda32a00e4ef0ed8a7
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 17:37:08 +1000

    Update definitions.ts

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -24,3 +24,11 @@ export type VoiceForm = {
     mouth: number;
     throat: number;
 }
+
+export type AccessToken = {
+    accessToken: string; //   The access token which is necessary for every request to the Twitch API.
+    expiresIn: number | null; //   The time, in seconds from the obtainment date, when the access token expires.
+    obtainmentTimeStamp: number; //   The date when the token was obtained, in epoch milliseconds.
+    refreshToken: string | null; //   The refresh token which is necessary to refresh the access token once it expires.
+    scrope: string[]; //   The scope the access token is valid for, i.e. what the token enables you to do.
+  }
