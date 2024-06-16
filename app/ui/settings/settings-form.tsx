7188563da116a5d863dc84d8a4a59fada102f6f2
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-16 21:00:04 +1000

    Update settings-form.tsx
    Changed initial values of voice settings, renamed savedSettings to savedVoices

--- app/ui/settings/settings-form.tsx
+++ app/ui/settings/settings-form.tsx
@@ -9,12 +9,13 @@ interface VoiceSettings {
 }
 
 const SettingsPage: React.FC = () => {
-    const [speed, setSpeed] = useState<number>(128);
-    const [pitch, setPitch] = useState<number>(128);
+    const [speed, setSpeed] = useState<number>(72);
+    const [pitch, setPitch] = useState<number>(64);
     const [mouth, setMouth] = useState<number>(128);
     const [throat, setThroat] = useState<number>(128);
     const [settingsName, setSettingsName] = useState<string>("");
-    const [savedSettings, setSavedSettings] = useState<{ [key: string]: VoiceSettings }>({});
+    const [savedVoices, setSavedVoices] = useState<{ [key: string]: VoiceSettings }>({});
+    const [enabledVoices, setEnabledVoices] = useState<{ [key: string]: boolean }>({});
     const [redeemName, setRedeemName] = useState<string>("");
     const [savedRedeems, setSavedRedeems] = useState<string[]>([]);
     const [enabledRedeems, setEnabledRedeems] = useState<{ [key: string]: boolean }>({});
@@ -25,7 +26,13 @@ const SettingsPage: React.FC = () => {
     useEffect(() => {
         const saved = localStorage.getItem("voiceSettings");
         if (saved) {
-            setSavedSettings(JSON.parse(saved));
+            setSavedVoices(JSON.parse(saved));
+        }
+
+        const savedEnabledVoices = localStorage.getItem("enabledVoices");
+
+        if (savedEnabledVoices) {
+            setEnabledVoices(JSON.parse(savedEnabledVoices));
         }
 
         const savedRedeems = localStorage.getItem("redeemSettings");
@@ -46,38 +53,60 @@ const SettingsPage: React.FC = () => {
         }
     }, []);
 
-    const saveSettings = () => {
+    const saveVoice = () => {
         if (settingsName.trim() === "") return;
 
         const newSettings = {
-            ...savedSettings,
-            [settingsName]: { speed, pitch, mouth, throat },
+            ...savedVoices,
+            [settingsName.toLowerCase().trim()]: { speed, pitch, mouth, throat },
         };
 
-        setSavedSettings(newSettings);
+        const newEnabledVoices = {
+            ...enabledVoices,
+            [settingsName.toLowerCase().trim()]: true,
+        };
+
+        setSavedVoices(newSettings);
+        setEnabledVoices({ ...enabledVoices, [settingsName]: true });
         localStorage.setItem("voiceSettings", JSON.stringify(newSettings));
+        localStorage.setItem("enabledVoices", JSON.stringify(newEnabledVoices));
         setSettingsName("");
     };
 
     const saveRedeem = () => {
-        if (redeemName.trim() === '') return;
+        if (redeemName.trim() === "") return;
 
-        const newRedeems = [...savedRedeems, redeemName];
+        const newRedeems = [...savedRedeems, redeemName.toLowerCase().trim()];
+
+        const newEnabledRedeems = {
+            ...enabledRedeems,
+            [redeemName.toLowerCase().trim()]: true,
+        };
+
         setSavedRedeems(newRedeems);
         setEnabledRedeems({ ...enabledRedeems, [redeemName]: true });
-        localStorage.setItem('redeemSettings', JSON.stringify(newRedeems));
-        setRedeemName('');
+        localStorage.setItem("redeemSettings", JSON.stringify(newRedeems));
+        localStorage.setItem("enabledRedeems", JSON.stringify(newEnabledRedeems));
+        setRedeemName("");
     };
 
     const saveCheerThreshold = () => {
-        localStorage.setItem('cheerThreshold', cheerThreshold.toString());
+        localStorage.setItem("cheerThreshold", cheerThreshold.toString());
         setSavedCheerThreshold(cheerThreshold);
     };
 
     const toggleRedeemEnabled = (redeem: string) => {
         const updatedEnabledRedeems = { ...enabledRedeems, [redeem]: !enabledRedeems[redeem] };
         setEnabledRedeems(updatedEnabledRedeems);
+        localStorage.setItem("enabledRedeems", JSON.stringify(updatedEnabledRedeems));
     };
+
+    const toggleVoiceEnabled = (name: string) => {
+        const updatedEnabledVoices = { ...enabledVoices, [name]: !enabledVoices[name] };
+        console.log(updatedEnabledVoices);
+        setEnabledVoices(updatedEnabledVoices);
+        localStorage.setItem("enabledVoices", JSON.stringify(updatedEnabledVoices));
+    }
 
     return (
         <div>
@@ -105,7 +134,7 @@ const SettingsPage: React.FC = () => {
                         min="0"
                         max="255"
                         value={pitch}
-                        className="w-full h-2 bg-black rounded-lg cursor-pointer dark:bg-gray-700 accent-linear-gradient(to right, #515151, #E6BFFF)"
+                        className="w-full h-2 rounded-lg cursor-pointer"
                         onChange={(e) => setPitch(parseInt(e.target.value))} />
                 </div>
                 <div className="relative mb-4">
@@ -140,16 +169,21 @@ const SettingsPage: React.FC = () => {
                     placeholder="Voice Name"
                     value={settingsName}
                     className="rounded-md border border-gray-200 w-11/12 p-1 mt-2 mr-2 text-m outline-2 placeholder:text-black text-black"
-                    onChange={(e) => setSettingsName(e.target.value)} />
+                    onChange={(e) => setSettingsName(e.target.value.toLowerCase().trim())} />
                 <button
-                    className="flex h-10 w-11/12 items-center rounded-lg bg-purple-800 px-4 text-sm font-medium text-white transition-colors hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-fuchsia-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
-                    onClick={saveSettings}>Save Voice</button>
+                    className="flex h-10 w-11/12 items-center rounded-lg bg-fuchsia-950 px-4 text-sm font-medium text-white transition-colors hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-fuchsia-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
+                    onClick={saveVoice}>Save Voice</button>
             </div>
-            <h2 className={`${inter.className} mb-4 text-l md:text-xl mt-10`}>Saved Voices</h2>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2 mb-4">
-                {Object.keys(savedSettings).map((name) => (
+            <h2 className={`${inter.className} mb-4 text-l md:text-xl mt-10 pb-2 border-b-2 border-fuchsia-950`}>Saved Voices</h2>
+            <div className="grid gap-6 sm:grid-cols-3 lg:grid-cols-3 mb-4">
+                {Object.keys(savedVoices).map((name) => (
                     <div key={name}>
-                        <input type="checkbox" id={name} name={name} className={`${inter.className} mb-4 text-m md:text-l`} />
+                        <input type="checkbox" 
+                        id={name} 
+                        name={name}
+                        checked={enabledVoices[name] || false}
+                        onChange={() => toggleVoiceEnabled(name)}
+                        className={`${inter.className} mb-4 mr-2 text-m md:text-l accent-fuchsia-950`} />
                         <label htmlFor={name}>{name}</label>
                     </div>
                 ))}
