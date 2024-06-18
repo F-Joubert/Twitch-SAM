62a86cfd047823ff5cea0eeab186df56f67c7142
Fergus <95570212+F-Joubert@users.noreply.github.com>
2024-06-19 07:16:46 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
               <div className="w-full flex-none md:w-64">
                   <SideNav />
-                  <TwitchChatListener />
+{/*                 Accidentally loaded <TwitchChatListener> twice here, since it's also a child component of SideNav */}
+{/*                   <TwitchChatListener /> */}
               </div>
               <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
           </div>
